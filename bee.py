f63fe4b5554e3aabf6b9951596cf823400f2dbe1
AliciaFdezFdez <100429161@alumnos.uc3m.es>
2022-11-24 23:43:09 +0100

    delete

--- bee.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-class Bee:
-
-    def __init__(self, pos, color):
-        self.pos = pos
-        self.color = color
--- board.py
+++
@@ -1,7 +0,0 @@
-import pygame
-
-class Board:
-
-    def __init__(self):
-        self.selected_piece = None
-        self.player1bees = self.player2bees = 6
